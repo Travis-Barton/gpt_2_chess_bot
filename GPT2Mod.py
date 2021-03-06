7904c1febcda528dd8a0b28397292297cab602dc
Travis j Barton <39603097+Travis-Barton@users.noreply.github.com>
2021-03-05 21:20:17 -0800

    order of opperations

--- GPT2Mod.py
+++ GPT2Mod.py
@@ -7,13 +7,9 @@ Created on Fri Jan 10 13:54:34 2020
 """
 # make sure you have tensorflow 1.15
 import gpt_2_simple as gpt2
-sess = gpt2.start_tf_sess()
-gpt2.load_gpt2(sess, run_name='new_run_large')
 gpt2.download_gpt2()
 
-
 sess = gpt2.start_tf_sess()
-gpt2.load_gpt2(sess, run_name='new_run_large')
 gpt2.finetune(sess,
               dataset='big_chess_set.txt',
               run_name='lets_play_chess',
